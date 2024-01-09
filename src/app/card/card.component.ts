973b7a3fb5b662e48b7bcff5aaeade781f054231
EdisonLopezEC <edisonlopezec@gmail.com>
2024-01-09 15:10:51 -0500

    portfolio v2.3

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  templateUrl: './card.component.html'
 })
 export class CardComponent {
   currentState = '#about';
